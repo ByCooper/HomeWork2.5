6d256512bf34455b4cbfe4ee86da104808467fb7
ByCooper <kup.official@yahoo.com>
2023-06-07 16:42:05 +0300

    Домашнее задание (скелет)

---
+++ src/main/java/ru/byCooper/employeeProject/EmployeeProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.byCooper.employeeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProjectApplication.class, args);
+	}
+
+}
